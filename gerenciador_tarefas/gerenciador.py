189ead54b7b343594602434594a7b8dbb1ebed9f
Strongreen <ka.ah19@hotmail.com>
2019-11-30 15:36:25 -0300

    Adicionou o gerenciador

--- gerenciador_tarefas/gerenciador.py
+++ gerenciador_tarefas/gerenciador.py
@@ -2,8 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-TAREFAS = []
+TAREFAS = [
+{"id": 1, "titulo" : "comprar ovos"},
+{"id": 2, "titulo": "comprar sabonete"}
+
+]
 
 @app.get('/tarefas')
 def listar():
     return TAREFAS
+
+@app.post('/Adicionar-Nova-Tarefas')
+def Adicionar(NovaTarefa):
+    NovaTarefa = "nova tarefa"
+
+@app.delete('/Concluir-Tarefas')
+def Adicionar(NovaTarefa):
+    NovaTarefa = "Excluir"
